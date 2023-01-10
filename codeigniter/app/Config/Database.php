4151a096aa96cebd04d872af43bbe0a58758447e
Leonard Richertz <leo.richertz@gmx.de>
2023-01-10 18:48:54 +0100

    Übungsblatt 6

---
+++ codeigniter/app/Config/Database.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Database\Config;
+
+/**
+ * Database Configuration
+ */
+class Database extends Config
+{
+    /**
+     * The directory that holds the Migrations
+     * and Seeds directories.
+     *
+     * @var string
+     */
+    public $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+
+    /**
+     * Lets you choose which connection group to
+     * use if no other is specified.
+     *
+     * @var string
+     */
+    public $defaultGroup = 'default';
+
+    /**
+     * The default database connection.
+     *
+     * @var array
+     */
+    public $tests = [
+        'DSN'      => '',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'uebungsentwicklung',
+        'DBDriver' => 'mysqli',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => (ENVIRONMENT !== 'production'),
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 3307,//wurde von 3006 zu 3007 geändert
+    ];
+
+    /**
+     * This database connection is used when
+     * running PHPUnit database tests.
+     *
+     * @var array
+     */
+
+    public $default = [
+        'DSN'         => '',
+        'hostname'    => 'sql96.your-server.de',
+        'username'    => 'leonarb_1',
+        'password'    => 'x9TEp1N82NYyLHcr',
+        'database'    => 'leonarb_db_todolist',
+        'DBDriver'    => 'mysql',
+        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+        'pConnect'    => false,
+        'DBDebug'     => (ENVIRONMENT !== 'production'),
+        'charset'     => 'utf8',
+        'DBCollat'    => 'utf8_general_ci',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
+        'foreignKeys' => true,
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Ensure that we always set the database group to 'tests' if
+        // we are currently running an automated test suite, so that
+        // we don't overwrite live data on accident.
+        if (ENVIRONMENT === 'testing') {
+            $this->defaultGroup = 'tests';
+        }
+    }
+}
